a9a6c44765c0311bcbae1ad06e02e8c735c48a83
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-08-08 00:32:48 -0400

    lab complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,39 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("pointsForWord", () => {
+    it("Should return true for words that are a combination of uppercase and lowercase letters (all lowercase)", () => {
+        const word = "racecar";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("Should return true for words that are a combination of uppercase and lowercase letters", () => {
+        const word = "RAceCar";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("Should return false for an empty string", () => {
+        const word = "";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    });
+    it("Should return false for non string inputs", () => {
+        const word = 1;
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    });
+    it("Should return false for invalid chars", () => {
+        const word = "!";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-// Your code here
+export function isPalindrome(word) {
+    if (/^[A-Za-z]+$/.test(word)) {
+        if (word.toLowerCase() === word.toLowerCase().split('').reverse().join('') && word !== '') {
+            return true
+        } else return false;
+    } else return false;
+}
